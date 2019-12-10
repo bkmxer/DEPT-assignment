6292745a0b21be5f7040e1ea2a08c51011450506
Daphne Smit <daphnesmit1986@gmail.com>
2019-12-10 14:02:32 +0100

    add note to examples

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -12,10 +12,12 @@ import '@components/image'
 import VideoLoader from '@components/video/loader'
 
 // Sync example
+// import moduleInit from '@utilities/module-init'
 // import Example from '@components/example' // Sync
 // moduleInit.sync('[js-hook-module-example]', Example) // Sync
 
 // Async example
+// import moduleInit from '@utilities/module-init'
 // moduleInit.async('[js-hook-module-example]', () => import('@components/example')) // Async
 
 VideoLoader.then(([Platforms, Video]) => {
